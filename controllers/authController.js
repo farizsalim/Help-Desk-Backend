e680921c740c03a3d50ddd4eebca38031c59f3fa
farizsalim <farizsalim10@yahoo.co.id>
2026-02-25 11:23:19 +0800

    ubah penggunaan token di localstorage (Peningkatan Security)

--- controllers/authController.js
+++ controllers/authController.js
@@ -173,7 +173,14 @@ exports.logout = (req, res) => {
 // @access  Private
 exports.getMe = async (req, res) => {
   try {
-    const user = await User.findById(req.user._id);
+    const user = await User.findById(req.user._id).select('-password');
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: 'User not found'
+      });
+    }
 
     res.status(200).json({
       success: true,
